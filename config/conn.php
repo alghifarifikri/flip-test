b1e3bed22f1f8a0f16fee778940044a4fd2b066e
alghifarifikri <alghi77@gmail.com>
2020-02-14 23:04:31 +0700

    first commit

---
+++ config/conn.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once './dbclass.php';
+    try 
+        {
+            $dbclass = new DBClass(); 
+            $connection = $dbclass.getConnection();
+            $sql = file_get_contents("migration/database.sql"); 
+            $connection->exec($sql);
+            echo "Database and tables created successfully!";
+        }
+    catch(PDOException $e)
+        {
+            echo $e->getMessage();
+        }
+
+?>
---
+++ disburse/entities/disbursement.php
@@ -0,0 +1,67 @@
+<?php
+class Disbursement{
+
+    private $connection;
+
+    private $table_name = "disbursements";
+
+    public $id;
+    public $amount;
+    public $status;
+    public $bank_code;
+    public $account_number;
+    public $beneficiary_name;
+    public $remark;
+    public $receipt;
+    public $time_served;
+    public $fee;
+
+    public function __construct($connection){
+        $this->connection = $connection;
+    }
+
+    //C
+    public function create(){
+        $query = 'INSERT INTO disbursements (amount, status, bank_code, account_number, beneficiary_name, remark, receipt, fee) VALUES (';
+        $query .= $this->amount . ', ';
+        $query .= $this->status . ', ';
+        $query .= $this->bank_code . ', ';
+        $query .= $this->account_number . ', ';
+        $query .= $this->beneficiary_name . ', ';
+        $query .= $this->remark . ', ';
+        $query .= $this->receipt . ', ';
+        $query .= $this->fee . ')';
+
+        $stmt = $this->connection->prepare($query);
+
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    //R
+    public function read(){
+        $query = 'SELECT * FROM'. $this->table_name .' WHERE id = ' . $this->id;
+
+        $stmt = $this->connection->prepare($query);
+
+        $stmt->execute();
+
+        return $stmt;
+        
+    }
+
+    //U
+    public function update(){
+        $query = 'UPDATE disbursements SET status = ' . $this->status . ', receipt = ' . $this->receipt . ' WHERE id = ' . $this->id;
+
+        $stmt = $this->connection->prepare($query);
+
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    //D
+    public function delete(){}
+}
